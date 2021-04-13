0a5d03863fbc64dd3d4c4dce65910df5a67cf78d
iemong <kiyoshidainagon@gmail.com>
2021-04-13 23:47:08 +0900

    web componentのオプションをtrueにする

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -4,6 +4,9 @@ const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	compilerOptions: {
+		customElement: true
+	},
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
